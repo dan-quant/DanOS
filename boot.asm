ebde8876b529d9e4791b055c696096f5da66f0ff
dan-quant <danmont@gmail.com>
2024-01-09 20:40:04 +0000

    further testing to the qemu, nasm and overall setup environment

--- boot.asm
+++ boot.asm
@@ -5,12 +5,31 @@
 ORG 0x7c00					;	this is the standard address where the BIOS loads the bootloader
 BITS 16						;	assembler will assemble instructions into 16-bit code (real mode is 16-bit only)
 
+
 start:
-	mov ah, 0x0e			; command to display a character on screen
-	mov al, 'A'				; the character to be displayed
-	mov bx, 0				; bh register holds the page number
-	int 0x10				; calls a BIOS routine to execute and display the character
+	mov si, message			; loads the address of the label message on the register si
+	call print_loop			; calls the function that will loop throughout the string printing it char by char
 	jmp $					; jumps to itself to prevent executing the boot signature a few lines below
+
+print_loop:
+	mov bx, 0				; bx register holds the page number
+	cld						; clears the direction flag to ensure the si register will be incremented after the lodsb
+.loop:
+	lodsb					; loads a byte from si into register al and increments lodsb (DF is 0)
+	cmp al, 0				; it is a null-terminated string, to this is checking if we have reached the end of the string
+	je .end					; if the string has ended, jumps to .end label to return
+	call print_char			; prints 1 character - the one loaded into al through lodsb
+	jmp .loop				; loops
+
+.end:
+	ret
+
+print_char:
+	mov ah, 0x0e			; command to display a character on screen
+	int 0x10				; calls a BIOS routine to execute and display the character
+	ret
+
+message: db 'Welcome to DanOS', 0	; creates a null-terminated string
 
 times 510 - ($-$$) db 0		; sets all bytes from the end of this program until byte 510 to zero
 
